89280cbd335cf74ac003e4bc7e97f9158e0ab33b
get_ <39654442+getch04@users.noreply.github.com>
2024-09-06 16:45:43 +0300

    fix: story section updated

--- apps/portal/src/app/components/stories/story.tsx
+++ apps/portal/src/app/components/stories/story.tsx
@@ -1,34 +1,70 @@
 "use client";
 
-import { Box, Button, Container, Grid, Text, Title } from "@mantine/core";
+import { Box, Button, Grid, Text, Title, Image } from "@mantine/core";
+import { IconPlayerPlayFilled } from "@tabler/icons-react";
 
 export function Story() {
   return (
-    <Container className="p-10">
-      <Grid>
-        <Grid.Col span="auto">1</Grid.Col>
-        <Grid.Col span="auto">
-          <Box className="flex flex-col justify-start items-start">
-            <Text>OUR STORY</Text>
-            <Title>
-              Advanced Dental Care <br /> Tailored just for You.
+    <Box className="px-36 py-10 bg-gray-50">
+      <Grid justify="flex-center" align="stretch">
+        <Grid.Col span={5} className="relative">
+          <Image
+            src={"docs.jpg"}
+            radius="lg"
+            className="rounded-8 w-full h-[400px] object-cover border-4 border-white"
+            alt="Doctor Image"
+          />
+          <Box className="absolute bottom-[-40px] right-[30px]">
+            <Image
+              src={"odontologia.jpg"}
+              radius="lg"
+              className="rounded-8 w-[200px] h-[200px] object-cover border-8 border-white shadow-md"
+              alt="Dental Image"
+            />
+          </Box>
+        </Grid.Col>
+
+        <Grid.Col span={7}>
+          <Box className="flex flex-col justify-start items-start space-y-4">
+            <Text className="text-primary-500 text-sm">OUR STORY</Text>
+            <Title className="text-primary-900 text-4xl">
+              Advanced Dental Care <br /> Tailored Just for You.
             </Title>
-            <Box className="flex flex-row justify-center items-center">
+            <Text className="text-gray-500 text-md leading-relaxed">
+              At our clinic, we’re redefining dental care with a focus on
+              advanced techniques and personalized treatment. Led by Dr. Helina,
+              our team is committed to ensuring every patient receives
+              exceptional care in a comfortable, welcoming environment.
+            </Text>
+            <Text className="italic text-gray-400">
+              “We offer reasonable pricing health care plans, insurance packages
+              to clients insurance packages to clients”.
+            </Text>
+            <Text className="text-primary-800 font-bold">
+              Helina, Chief Doctor
+            </Text>
+
+            <Box className="flex flex-row justify-start items-center space-x-4 pt-4">
               <Button
-                color="var(--mantine-color-primary-7)"
                 size="md"
-                className="rounded-md px-72"
-                variant="Filled"
+                radius="md"
+                className="bg-primary-500 text-white hover:bg-primary-600 transition"
               >
-                <Text className="px-5">Read More</Text>
+                Read More
               </Button>
-            </Box>
-            <Box className="flex flex-row">
-              <Container className="rounded-md"></Container>
+              <Box className="flex items-center">
+                <Box className="rounded-full border-2 border-primary-200 p-2 flex justify-center items-center">
+                  <IconPlayerPlayFilled
+                    size={18}
+                    className="text-primary-900"
+                  />
+                </Box>
+                <Text className="pl-2">Watch Video</Text>
+              </Box>
             </Box>
           </Box>
         </Grid.Col>
       </Grid>
-    </Container>
+    </Box>
   );
 }
